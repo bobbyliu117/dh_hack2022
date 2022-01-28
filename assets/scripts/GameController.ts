d4bb1f1abda84d7a2d25d53addbf6599698e834c
Chang Liu <bobbyliu117@gmail.com>
2022-01-27 22:57:19 -0800

    add joystick script

--- assets/scripts/GameController.ts
+++ assets/scripts/GameController.ts
@@ -1,8 +1,11 @@
 
 import { _decorator, Component, Node } from 'cc';
+import { UIJoystick } from './UiJoystick';
 const { ccclass, property } = _decorator;
  
 @ccclass('GameController')
 export class GameController extends Component {
-  
+  onLoad() {
+		UIJoystick.init(this.node.parent.getChildByName("Canvas"));
+	}
 }
--- assets/scripts/UiJoystick.ts
+++ assets/scripts/UiJoystick.ts
@@ -1,44 +1,65 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, instantiate, resources, Prefab, EventTouch, v2, v3, Vec2, Vec3, UITransformComponent } from 'cc';
 const { ccclass, property } = _decorator;
 
-/**
- * Predefined variables
- * Name = UiJoystick
- * DateTime = Thu Jan 27 2022 22:50:40 GMT-0800 (Pacific Standard Time)
- * Author = bobbyliu117
- * FileBasename = UiJoystick.ts
- * FileBasenameNoExtension = UiJoystick
- * URL = db://assets/scripts/UiJoystick.ts
- * ManualUrl = https://docs.cocos.com/creator/3.3/manual/en/
- *
- */
+const HORIZONTAL = v2(1, 0);
  
-@ccclass('UiJoystick')
-export class UiJoystick extends Component {
-    // [1]
-    // dummy = '';
+@ccclass('UIJoystick')
+export class UIJoystick extends Component {
 
-    // [2]
-    // @property
-    // serializableDummy = 0;
+	static init(root: Node) {
+		resources.load("prefabs/ui/joystick/joystick", Prefab, (err, prefab) => {
+			if (err) {
+				console.log('= = => UIJoystick.init Error:', err);
+				return;
+			}
+			root.addChild(instantiate(prefab));
+		});
+	}
 
-    start () {
-        // [3]
-    }
+	private stick: Node;
+	private player: Node;
+	private camera: Node;
+	private tempV2 = v2(0, 0);
+	private tempV3 = v3(0, 0);
 
-    // update (deltaTime: number) {
-    //     // [4]
-    // }
+	onLoad() {
+		this.stick = this.node.getChildByName("stick");
+		this.player = this.node.parent.parent.getChildByName("haigui");
+		this.camera = this.node.parent.parent.getChildByName("Main Camera");
+
+		this.node.on(Node.EventType.TOUCH_START,(evTouch: EventTouch) => {
+			
+		});
+
+		this.node.on(Node.EventType.TOUCH_MOVE, (evTouch) => {
+			evTouch.getUILocation(this.tempV2);
+			this.tempV3.set(this.tempV2.x, this.tempV2.y, 0);
+
+			this.tempV3 = this.node.getComponent(UITransformComponent).convertToNodeSpaceAR(this.tempV3);
+			this.tempV3 = this.tempV3.length() > 80 ? this.tempV3.normalize().multiplyScalar(80): this.tempV3;
+
+			this.tempV2.set(this.tempV3.x, this.tempV3.y);
+			const angle = this.tempV2.angle(HORIZONTAL) * Math.sign(this.tempV2.y);
+			console.log(Math.cos(angle), Math.sin(angle));
+
+			this.stick.setPosition(this.tempV3);
+			this.player.setRotationFromEuler(0,180*angle/Math.PI,0);
+			
+			this.player.getWorldPosition(this.tempV3);
+			this.tempV3.x += (Math.cos(angle)*0.1);
+			this.tempV3.z -= (Math.sin(angle)*0.1);
+			this.player.setWorldPosition(this.tempV3);
+		})
+
+		this.node.on(Node.EventType.TOUCH_CANCEL, () => {
+			this.stick.setPosition(v3(0, 0, 0));
+		});
+
+		this.node.on(Node.EventType.TOUCH_END, () => {
+			this.stick.setPosition(v3(0, 0, 0));
+		})
+	}
+
+	
 }
-
-/**
- * [1] Class member could be defined like this.
- * [2] Use `property` decorator if your want the member to be serializable.
- * [3] Your initialization goes here.
- * [4] Your update function goes here.
- *
- * Learn more about scripting: https://docs.cocos.com/creator/3.3/manual/en/scripting/
- * Learn more about CCClass: https://docs.cocos.com/creator/3.3/manual/en/scripting/ccclass.html
- * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.3/manual/en/scripting/life-cycle-callbacks.html
- */
