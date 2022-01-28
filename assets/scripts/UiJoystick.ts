7bcdbc0e2bd7e33de5e53b0559c5a40c45839203
Chang Liu <bobbyliu117@gmail.com>
2022-01-27 23:14:38 -0800

    fix move pausing

--- assets/scripts/UiJoystick.ts
+++ assets/scripts/UiJoystick.ts
@@ -3,6 +3,8 @@ import { _decorator, Component, Node, instantiate, resources, Prefab, EventTouch
 const { ccclass, property } = _decorator;
 
 const HORIZONTAL = v2(1, 0);
+const SPEED = 0.1;
+const RING_R = 80;
  
 @ccclass('UIJoystick')
 export class UIJoystick extends Component {
@@ -20,50 +22,56 @@ export class UIJoystick extends Component {
 	private stick: Node;
 	private player: Node;
 	private camera: Node;
-	private tempV2 = v2(0, 0);
-	private tempV3 = v3(0, 0);
+	private tempV2 = v2();
+	private tempV3 = v3();
+	private updateV3 = v3();
+	private isMoving = false;
+	private angle = 0;
 
 	onLoad() {
 		this.stick = this.node.getChildByName("stick");
 		this.player = this.node.parent.parent.getChildByName("haigui");
 		this.camera = this.node.parent.parent.getChildByName("Main Camera");
 
-		this.node.on(Node.EventType.TOUCH_START,(evTouch: EventTouch) => {
-			
-		});
-
-		this.node.on(Node.EventType.TOUCH_MOVE, (evTouch) => {
-			evTouch.getUILocation(this.tempV2);
-			this.tempV3.set(this.tempV2.x, this.tempV2.y, 0);
-
-			this.tempV3 = this.node.getComponent(UITransformComponent).convertToNodeSpaceAR(this.tempV3);
-			this.tempV3 = this.tempV3.length() > 80 ? this.tempV3.normalize().multiplyScalar(80): this.tempV3;
-
-			this.tempV2.set(this.tempV3.x, this.tempV3.y);
-			const angle = this.tempV2.angle(HORIZONTAL) * Math.sign(this.tempV2.y);
-
-			this.stick.setPosition(this.tempV3);
-			this.player.setRotationFromEuler(0,180*angle/Math.PI,0);
-			
-			this.player.getWorldPosition(this.tempV3);
-			this.tempV3.x += (Math.cos(angle)*0.1);
-			this.tempV3.z -= (Math.sin(angle)*0.1);
-			this.player.setWorldPosition(this.tempV3);
-
-			this.camera.getWorldPosition(this.tempV3);
-			this.tempV3.x += (Math.cos(angle)*0.1);
-			this.tempV3.z -= (Math.sin(angle)*0.1);
-			this.camera.setWorldPosition(this.tempV3);
-		})
-
-		this.node.on(Node.EventType.TOUCH_CANCEL, () => {
-			this.stick.setPosition(v3(0, 0, 0));
-		});
-
-		this.node.on(Node.EventType.TOUCH_END, () => {
-			this.stick.setPosition(v3(0, 0, 0));
-		})
+		this.node.on(Node.EventType.TOUCH_START, this.onTouchStart);
+		this.node.on(Node.EventType.TOUCH_MOVE, this.onTouchMove);
+		this.node.on(Node.EventType.TOUCH_CANCEL, this.onTouchEnd);
+		this.node.on(Node.EventType.TOUCH_END, this.onTouchEnd);
 	}
 
+	private onTouchStart = () => this.isMoving = true
+
+	private onTouchMove = (evTouch: EventTouch) => {
+		evTouch.getUILocation(this.tempV2);
+		this.tempV3.set(this.tempV2.x, this.tempV2.y, 0);
+
+		this.tempV3 = this.node.getComponent(UITransformComponent).convertToNodeSpaceAR(this.tempV3);
+		this.tempV3 = this.tempV3.length() > RING_R ? this.tempV3.normalize().multiplyScalar(RING_R): this.tempV3;
+
+		this.tempV2.set(this.tempV3.x, this.tempV3.y);
+		this.angle = this.tempV2.angle(HORIZONTAL) * Math.sign(this.tempV2.y);
+
+		this.stick.setPosition(this.tempV3);
+		this.player.setRotationFromEuler(0,180*this.angle/Math.PI,0);
+	}
+
+	private onTouchEnd = () => {
+		this.stick.setPosition(v3(0, 0, 0));
+			this.isMoving = false;
+	}
+
+	update(dt: number) {
+		if (this.isMoving) {
+			this.player.getWorldPosition(this.updateV3);
+			this.updateV3.x += (Math.cos(this.angle)*SPEED);
+			this.updateV3.z -= (Math.sin(this.angle)*SPEED);
+			this.player.setWorldPosition(this.updateV3);
+
+			this.camera.getWorldPosition(this.updateV3);
+			this.updateV3.x += (Math.cos(this.angle)*SPEED);
+			this.updateV3.z -= (Math.sin(this.angle)*SPEED);
+			this.camera.setWorldPosition(this.updateV3);
+		}
+	}
 	
 }
